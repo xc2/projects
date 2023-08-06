fae897c3edb3a422b738c2fad9d425cbf9a11608
xc2 <xc2@noreply.github.com>
2023-08-07 01:18:45 +0800

    feat: fluentui integration

--- watermark/src/App.tsx
+++ watermark/src/App.tsx
@@ -1,7 +1,55 @@
+import {
+  Checkbox,
+  Combobox,
+  Field,
+  Input,
+  Option,
+  Radio,
+  RadioGroup,
+  Slider,
+  SpinButton,
+  Switch,
+  Textarea,
+} from "@fluentui/react-components";
+
 export function App() {
   return (
-    <div className="">
-      <div className="pl-3 pr-3 pt-2 pb-2">a</div>
+    <div className="flex-row space-y-3">
+      <h1 className="text-2xl pl-3 pr-3 pt-6">Make watermark</h1>
+      <div className="pl-3 pr-3 ">
+        <Field label="Input">
+          <Input />
+        </Field>
+        <Field label="Textarea">
+          <Textarea />
+        </Field>
+        <Field label="Combobox">
+          <Combobox>
+            <Option>Option 1</Option>
+            <Option>Option 2</Option>
+            <Option>Option 3</Option>
+          </Combobox>
+        </Field>
+        <Field label="SpinButton">
+          <SpinButton />
+        </Field>
+        <Field hint="Checkboxes use their own label instead of the Field label.">
+          <Checkbox label="Checkbox" />
+        </Field>
+        <Field label="Slider">
+          <Slider defaultValue={25} />
+        </Field>
+        <Field label="Switch">
+          <Switch />
+        </Field>
+        <Field label="RadioGroup">
+          <RadioGroup>
+            <Radio label="Option 1" />
+            <Radio label="Option 2" />
+            <Radio label="Option 3" />
+          </RadioGroup>
+        </Field>
+      </div>
     </div>
   );
 }
--- watermark/src/main.tsx
+++ watermark/src/main.tsx
@@ -2,5 +2,10 @@ import "./globals.css";
 import "./main.css";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <FluentProvider theme={teamsLightTheme}>
+    <App />
+  </FluentProvider>,
+);
